c3b51ea7b572fbf1897494e6a7c6652ab0d4d06c
Victor Correa <vcorrea90@gmail.com>
2020-12-12 12:46:07 +0100

    Vistas básicas

---
+++ app/Views/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>feedNews</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+        <a href="index.php" class="my-0 mr-md-auto">
+            <h5 class="font-weight-normal">feedNews</h5>
+        </a>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="administrator.php">Administrador</a>
+        </nav>
+
+    </header>
+    <main class="container">
